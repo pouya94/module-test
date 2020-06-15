2f61241250c4a332847f5b096129381829cbe448
pouya <pouya.yosefnejad@zoodfood.com>
2020-06-15 10:59:46 +0430

    Blank fragment added

---
+++ ModuleA/src/main/java/ir/snappfood/modulea/MyFragment.java
@@ -0,0 +1,28 @@
+package ir.snappfood.modulea;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class MyFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.blank, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
